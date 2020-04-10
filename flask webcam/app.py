c4a2a0d71ca1b3565c0f03a981f557614b4e4f25
Theis Eizo <theizo.bedsted@gmail.com>
2020-04-10 19:35:38 +0200

    added flask webcam easy setup

---
+++ flask webcam/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        ret, frame = camera.read()
+        cv2.imwrite('temp_video_feed.jpg', cv2.flip(frame, 1))
+        yield (b'--frame\r\n' 
+              b'Content-Type: image/jpeg\r\n\r\n' 
+                  + open('temp_video_feed.jpg', 'rb').read() 
+                  + b'\r\n') 
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(cv2.VideoCapture(0)), 
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
