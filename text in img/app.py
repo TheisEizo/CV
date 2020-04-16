13c835af1889e6e8d97c5b6527a6762905f60ad3
Theis Eizo <theizo.bedsted@gmail.com>
2020-04-16 19:43:55 +0200

    minor changes to app

--- text in img/app.py
+++ text in img/app.py
@@ -1,30 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 15 14:00:15 2020
-
-@author: Theis Eizo
-"""
-
-from wand.image import Image
-
-with Image(filename='sample_doc.pdf') as img:
-
-    print('width =', img.width)
-    print('height =', img.height)
-    print('pages = ', len(img.sequence))
-    print('resolution = ', img.resolution)
-
-with img.convert('png') as converted:
-     converted.save(filename='sample_doc.png')
-
-#%%
+#%% IMPORT AND SETUP
+import pdf2image as p2i
+import os
 import PIL
 import pytesseract
 
-TESSDATA_PREFIX = r'C:\Users\45414\AppData\Local\Tesseract-OCR'
+pytesseract.pytesseract.tesseract_cmd = (
+    r'C:\Users\45414\AppData\Local\Tesseract-OCR\tesseract')
 
-pytesseract.pytesseract.tesseract_cmd = TESSDATA_PREFIX+'\\tesseract'
+name = 'test'
 
-img = PIL.Image.open('sample_doc.png').convert("RGB")
-output = pytesseract.image_to_string(img, lang='dan')
-print(output)
+#%% CONVERT PDF PAGES TO PNG FILES IN FOLDER
+first = 0 #page
+last   = 9 #page
+
+pages = p2i.convert_from_path(f'{name}.pdf',
+                              first_page=first, last_page=last)
+
+if f'{name}' not in os.listdir():
+    os.mkdir(f'{name}')
+
+if not first: first = 0
+for n, page in enumerate(pages):
+    page.save(f'{name}/{name}_{n+first}.png', 'png')
+    
+#%% EXCTRAT TEXT FROM PNG
+output = {}
+for path in os.listdir(f'{name}'):
+    img = PIL.Image.open(f'{name}/{path}').convert("RGB")
+    output[path.split('.')[0].split('_')[1]] = (
+        pytesseract.image_to_string(img, lang='dan'))
+   
